93956782f7bfa8ff5e95455de5a86ae1332907e1
rfxlamia <acaciaavante@gmail.com>
2026-09-17 23:49:36 +0700

    refactor(architecture): seed features/tracker detail chrome
    Move Changelog, DateFields, Properties, and PhaseEditor into the mapped
    tracker feature with leftover stubs so oversized pages keep default imports
    and named types (PropertyPatch, PhaseEditorValues) without a 300-on-touch binding change.

--- client/src/features/tracker/index.ts
+++ client/src/features/tracker/index.ts
@@ -1,8 +1,14 @@
+export { default as TrackerChangelog } from "./TrackerChangelog";
 export { default as TrackerConfirmDialog } from "./TrackerConfirmDialog";
+export { default as TrackerDateFields } from "./TrackerDateFields";
+export { default as TrackerPhaseEditor } from "./TrackerPhaseEditor";
+export type { PhaseEditorValues } from "./TrackerPhaseEditor";
 export { default as TrackerProgressBar } from "./TrackerProgressBar";
 export { default as TrackerProjectCard } from "./TrackerProjectCard";
 export { default as TrackerProjectCreateModal } from "./TrackerProjectCreateModal";
 export { default as TrackerProjectHeader } from "./TrackerProjectHeader";
 export { default as TrackerProjectsTab } from "./TrackerProjectsTab";
+export { default as TrackerProperties } from "./TrackerProperties";
+export type { PropertyPatch } from "./TrackerProperties";
 export { default as TrackerTabs } from "./TrackerTabs";
 export type { TrackerTab } from "./TrackerTabs";
